2f3aff3410f4ebcaf9e24317d70004991d7ab825
yohannesclaudealvin <76873435+yohannesclaudealvin@users.noreply.github.com>
2024-05-20 15:44:57 +0200

    Update ETP_Streamlit.py

--- ETP_Streamlit.py
+++ ETP_Streamlit.py
@@ -521,7 +521,7 @@ def build_model(data, Methode):
             donnee = pd.DataFrame({col1:list1})
             donnee.to_excel('fichier_result.xlsx',sheet_name = 'sheet1', index=False)
             st.markdown('Le fichier a été telecharger avec succès')
-            show(Result)
+            st.write(Result)
         st.markdown("## **📈 Affichage du graphe**")
 
         if st.form_submit_button('Appuyer pour afficher la représentations graphique'):
